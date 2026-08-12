8effb670f0043b60bb624b77d4821a28bef62a7f
Rich Hamilton <51587867+rhamenator@users.noreply.github.com>
2026-08-12 15:11:29 -0400

    Clarify GeoIP load context

--- src/integrations/geoip.rs
+++ src/integrations/geoip.rs
@@ -132,12 +132,10 @@ impl GeoipClient {
 fn open_optional(path: Option<&String>, database_name: &str) -> Result<Option<MmdbReader>> {
     path.map(|path| {
         Reader::open_readfile(path)
-            .with_context(|| {
-                format!(
-                    "failed to load configured {} MMDB at {}",
-                    database_name, path
-                )
-            })
+            .context(format!(
+                "failed to load configured {} MMDB at {}",
+                database_name, path
+            ))
             .map(Arc::new)
     })
     .transpose()
